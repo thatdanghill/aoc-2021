f267d446d8867e80e5dd656404a2eb6ab83a4fd1
Daniel Hill <thatdanghill@gmail.com>
2021-12-02 13:32:06 -0800

    Shorten

--- d1/solution.py
+++ d1/solution.py
@@ -1,7 +1,5 @@
-import numpy as n
-p=print
-a=n.array([int(l)for l in open('i')])
-p(((a[1:]-a[:-1]) > 0).sum())
+import sys,numpy as n
+a=n.array([int(l)for l in open(sys.argv[1])])
 b=n.convolve(a,n.ones(3),'valid')
-p(((b[1:]-b[:-1]) > 0).sum())
+print(f'silver: {((a[1:]-a[:-1])>0).sum()}\ngold: {((b[1:]-b[:-1])>0).sum()}')
 
