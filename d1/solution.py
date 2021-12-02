b7e8d804ea4e143eaefe9e9139c2f1146a71db06
Daniel Hill <thatdanghill@gmail.com>
2021-12-02 08:44:55 -0800

    Day 2 and shorten day 1

--- d1/solution.py
+++ d1/solution.py
@@ -1,8 +1,8 @@
-import numpy as np
-
-with open('input.txt', 'r') as f:
-    a = np.array([int(l) for l in f.readlines()])
-print(((a[1:]-a[:-1]) > 0).sum())
-b=np.convolve(a,np.ones(3),'valid')
-print(((b[1:]-b[:-1]) > 0).sum())
+import numpy as n
+p=print
+with open('i.txt', 'r')as f:
+    a=n.array([int(l)for l in f.readlines()])
+p(((a[1:]-a[:-1]) > 0).sum())
+b=n.convolve(a,n.ones(3),'valid')
+p(((b[1:]-b[:-1]) > 0).sum())
 
---
+++ d2/solution.py
@@ -0,0 +1,13 @@
+import numpy as n
+p=print
+with open('i.txt','r')as f:
+    a=[l.split(' ')for l in f.readlines()]
+    a=[(l[0], int(l[1]))for l in a]
+f='forward'
+d='down'
+x=sum([(0,v)[c==f]for c,v in a])
+m=[((-v,v)[c==d],0)[c==f]for c,v in a]
+y=sum(m)
+p(x*y)
+y=sum(n.cumsum(m)*[(0,v)[c==f] for c,v in a])
+p(x*y)
