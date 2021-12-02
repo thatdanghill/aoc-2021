98cbfc287981df5b7069f7ca2a7579e26a9cc255
Daniel Hill <thatdanghill@gmail.com>
2021-12-02 08:47:14 -0800

    Shorten

--- d1/solution.py
+++ d1/solution.py
@@ -1,7 +1,7 @@
 import numpy as n
 p=print
-with open('i.txt', 'r')as f:
-    a=n.array([int(l)for l in f.readlines()])
+f=open('i.txt')
+a=n.array([int(l)for l in f.readlines()])
 p(((a[1:]-a[:-1]) > 0).sum())
 b=n.convolve(a,n.ones(3),'valid')
 p(((b[1:]-b[:-1]) > 0).sum())
