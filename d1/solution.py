d89a748b56b546bc3de3e7b60fafc48e9ce6f5ce
Daniel Hill <thatdanghill@gmail.com>
2021-12-02 12:09:54 -0800

    Shorten

--- d1/solution.py
+++ d1/solution.py
@@ -1,6 +1,6 @@
 import numpy as n
 p=print
-a=n.array([int(l)for l in open('i.txt')])
+a=n.array([int(l)for l in open('i')])
 p(((a[1:]-a[:-1]) > 0).sum())
 b=n.convolve(a,n.ones(3),'valid')
 p(((b[1:]-b[:-1]) > 0).sum())
--- d2/solution.py
+++ d2/solution.py
@@ -1,14 +1,11 @@
 import numpy as n
 p=print
 s=sum
-a=[l.split()for l in open('i.txt')]
+a=[l.split()for l in open('i')]
 a=[(c,int(v))for c,v in a]
 f='forward'
 d='down'
 z=[(0,v)[c==f]for c,v in a]
-x=s(z)
 m=[((-v,v)[c==d],0)[c==f]for c,v in a]
-y=s(m)
-p(x*y)
-y=s(n.cumsum(m)*z)
-p(x*y)
+p(s(z)*s(m))
+p(s(z)*s(n.cumsum(m)*z))
