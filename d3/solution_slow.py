86c4fa075a61d8f83c248f72252e96277b05942e
Daniel Hill <thatdanghill@gmail.com>
2021-12-03 13:59:51 -0800

    Add slow solution

--- d3/solution_slow.py
+++ d3/solution_slow.py
@@ -7,7 +7,6 @@ y=max,-1
 z=min,1
 def f(x,l=a):
  for i in r:
-  n=v(*x,l)[i]
-  l=[j for j in l if n==j[i]]
+  l=[j for j in l if v(*x,l)[i]==j[i]]
   if len(l)<2:return s(l[0])
 print(f'Silver: {s(v(*y,a))*s(v(*z,a))}\nGold: {f(y)*f(z)}')
