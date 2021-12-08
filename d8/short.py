b876a85bb8b87d723db8e328fdf60197b44d55c7
Daniel Hill <thatdanghill@gmail.com>
2021-12-08 11:47:09 -0800

    Shorten

--- d8/short.py
+++ d8/short.py
@@ -7,10 +7,9 @@ r={2:1,3:7,4:4,7:8}
 for S,D in A:
  M={}
  for s in S:M[r.get(L(s))]=v(s)
- c,f=M[1]
- b,d={*M[4]}-{c,f}
+ m={*M[1]}
  for s in S:
-  s,Y,Z=v(s),(b in s)*(d in s),(c in s)*(f in s)
+  s,Y,Z=v(s),~L({*M[4]}-m-{*s})+2,~L(m-{*s})+2
   M[((10,((0,9)[Y],6)[~Z+2])[L(s)==6],((2,5)[Y],3)[Z])[L(s)==5]]=s
- O+=int(''.join([str({i:j for j,i in M.items() if j!=10}[v(k)])for k in D]))
+ O+=int(''.join([str({i:j for j,i in M.items()if j!=10}[v(k)])for k in D]))
 print(f'Silver: {sum([L(j)in[2,3,4,7]for _,i in A for j in i])}\nGold: {O}')
