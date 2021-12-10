776ce5ceb2262eec679dd1f3e0747971dd394f87
Daniel Hill <thatdanghill@gmail.com>
2021-12-10 09:26:05 -0800

    Shorten

--- d10/short.py
+++ d10/short.py
@@ -1,19 +1,13 @@
 import sys
-O={'{':'}','[':']','(':')','<':'>'}
-P={')':3,']':57,'}':1197,'>':25137}
-Q={')':1,']':2,'}':3,'>':4}
+O,P,*G='([{<',')]}>'
+Q=[3,57,1197,25137]
 S=0
-G=[]
 for A in open(sys.argv[1]):
- C=[]
- t=g=0
+ t,g,*C=0,0
  for c in A[:-1]:
-  if c in O:C=[O[c]]+C
-  elif c!=C.pop(0):
-   S+=P[c]
-   t=1
-   break
- if ~t+2:
-  for c in C:g=g*5+Q[c]
+  if c in O:C=[P[O.index(c)]]+C
+  elif c!=C.pop(0):S,t=S+Q[P.index(c)],1
+ if~t+2:
+  for c in C:g=g*5+P.index(c)+1
   G+=[g]
 print(f'Silver: {S}\nGold: {sorted(G)[len(G)//2]}')
